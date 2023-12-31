9e2a4594e3275c418b2ad36592386a8b8a35da70
Claudio Rivera <me@claudiorivera.com>
2023-12-31 10:53:09 -0600

    ✨ Finish day 5, part 1

---
+++ 05/getDestinationFromSource.test.ts
@@ -0,0 +1,77 @@
+import { expect, it } from "bun:test";
+import { SEED_MAP_KEYS, getDestinationFromSource } from "./solvePart1";
+
+it("should return soil from seed", () => {
+  const seedToSoilMaps = [
+    new Map([
+      [SEED_MAP_KEYS.DESTINATION_RANGE_START, 50],
+      [SEED_MAP_KEYS.SOURCE_RANGE_START, 98],
+      [SEED_MAP_KEYS.RANGE_LENGTH, 2],
+    ]),
+    new Map([
+      [SEED_MAP_KEYS.DESTINATION_RANGE_START, 52],
+      [SEED_MAP_KEYS.SOURCE_RANGE_START, 50],
+      [SEED_MAP_KEYS.RANGE_LENGTH, 48],
+    ]),
+  ];
+
+  expect(getDestinationFromSource(79, seedToSoilMaps)).toEqual(81);
+  expect(getDestinationFromSource(14, seedToSoilMaps)).toEqual(14);
+  expect(getDestinationFromSource(55, seedToSoilMaps)).toEqual(57);
+  expect(getDestinationFromSource(13, seedToSoilMaps)).toEqual(13);
+});
+
+it("should return fertilizer from soil", () => {
+  const soilToFertilizerMaps = [
+    new Map([
+      [SEED_MAP_KEYS.DESTINATION_RANGE_START, 0],
+      [SEED_MAP_KEYS.SOURCE_RANGE_START, 15],
+      [SEED_MAP_KEYS.RANGE_LENGTH, 37],
+    ]),
+    new Map([
+      [SEED_MAP_KEYS.DESTINATION_RANGE_START, 37],
+      [SEED_MAP_KEYS.SOURCE_RANGE_START, 52],
+      [SEED_MAP_KEYS.RANGE_LENGTH, 2],
+    ]),
+    new Map([
+      [SEED_MAP_KEYS.DESTINATION_RANGE_START, 39],
+      [SEED_MAP_KEYS.SOURCE_RANGE_START, 0],
+      [SEED_MAP_KEYS.RANGE_LENGTH, 15],
+    ]),
+  ];
+
+  expect(getDestinationFromSource(81, soilToFertilizerMaps)).toEqual(81);
+  expect(getDestinationFromSource(14, soilToFertilizerMaps)).toEqual(53);
+  expect(getDestinationFromSource(57, soilToFertilizerMaps)).toEqual(57);
+  expect(getDestinationFromSource(13, soilToFertilizerMaps)).toEqual(52);
+});
+
+it("should return water from fertilizer", () => {
+  const fertilizerToWaterMaps = [
+    new Map([
+      [SEED_MAP_KEYS.DESTINATION_RANGE_START, 49],
+      [SEED_MAP_KEYS.SOURCE_RANGE_START, 53],
+      [SEED_MAP_KEYS.RANGE_LENGTH, 8],
+    ]),
+    new Map([
+      [SEED_MAP_KEYS.DESTINATION_RANGE_START, 0],
+      [SEED_MAP_KEYS.SOURCE_RANGE_START, 11],
+      [SEED_MAP_KEYS.RANGE_LENGTH, 42],
+    ]),
+    new Map([
+      [SEED_MAP_KEYS.DESTINATION_RANGE_START, 42],
+      [SEED_MAP_KEYS.SOURCE_RANGE_START, 0],
+      [SEED_MAP_KEYS.RANGE_LENGTH, 7],
+    ]),
+    new Map([
+      [SEED_MAP_KEYS.DESTINATION_RANGE_START, 57],
+      [SEED_MAP_KEYS.SOURCE_RANGE_START, 7],
+      [SEED_MAP_KEYS.RANGE_LENGTH, 4],
+    ]),
+  ];
+
+  expect(getDestinationFromSource(81, fertilizerToWaterMaps)).toEqual(81);
+  expect(getDestinationFromSource(53, fertilizerToWaterMaps)).toEqual(49);
+  expect(getDestinationFromSource(57, fertilizerToWaterMaps)).toEqual(53);
+  expect(getDestinationFromSource(52, fertilizerToWaterMaps)).toEqual(41);
+});
--- 05/solvePart1.ts
+++ 05/solvePart1.ts
@@ -1,38 +1,92 @@
+const SEED_MAP_TYPES = {
+  SEED_TO_SOIL: "seed-to-soil",
+  SOIL_TO_FERTILIZER: "soil-to-fertilizer",
+  FERTILIZER_TO_WATER: "fertilizer-to-water",
+  WATER_TO_LIGHT: "water-to-light",
+  LIGHT_TO_TEMPERATURE: "light-to-temperature",
+  TEMPERATURE_TO_HUMIDITY: "temperature-to-humidity",
+  HUMIDITY_TO_LOCATION: "humidity-to-location",
+} as const;
+
+export const SEED_MAP_KEYS = {
+  DESTINATION_RANGE_START: "destinationRangeStart",
+  SOURCE_RANGE_START: "sourceRangeStart",
+  RANGE_LENGTH: "rangeLength",
+} as const;
+
+type SeedMapType = (typeof SEED_MAP_TYPES)[keyof typeof SEED_MAP_TYPES];
+type SeedMapKey = (typeof SEED_MAP_KEYS)[keyof typeof SEED_MAP_KEYS];
+type SeedMap = Map<SeedMapKey, number>;
+
 export function solvePart1(rows: Array<string>) {
-  const seeds = rows[0].split(":")[1].split(" ").filter(Boolean).map(Number);
+  const seeds = rows[0].split(": ")[1].split(" ").map(Number);
 
-  let currentMapKey: string;
+  const map = makeMap(rows.slice(2));
 
-  const map = rows
-    .slice(2)
-    .filter(Boolean)
-    .reduce((map, currentRow) => {
-      if (currentRow.includes(" map:")) {
-        [currentMapKey] = currentRow.split(" map:");
-        map.set(currentMapKey, []);
-        return map;
-      }
+  return seeds
+    .map((seed) =>
+      Object.values(SEED_MAP_TYPES).reduce((destination, seedMapType) => {
+        const seedMap = map.get(seedMapType);
 
-      const [destinationRangeStart, sourceRangeStart, rangeLength] = currentRow
-        .split(/\s/)
-        .map(Number);
+        if (!seedMap) throw new Error("Unreachable");
 
-      const currentMapKeyMaps = map.get(currentMapKey);
+        return getDestinationFromSource(destination, seedMap);
+      }, seed)
+    )
+    .reduce((min, seedLocation) => {
+      return Math.min(min, seedLocation);
+    }, Infinity);
+}
 
-      if (currentMapKeyMaps) {
-        currentMapKeyMaps.push(
-          new Map([
-            ["destinationRangeStart", destinationRangeStart],
-            ["sourceRangeStart", sourceRangeStart],
-            ["rangeLength", rangeLength],
-          ])
-        );
-      }
+function makeMap(rows: Array<string>) {
+  let currentSeedMapType: SeedMapType;
 
+  return rows.filter(Boolean).reduce((map, currentRow) => {
+    if (currentRow.includes(" map:")) {
+      currentSeedMapType = currentRow.split(" map:")[0] as SeedMapType;
+      map.set(currentSeedMapType, []);
       return map;
-    }, new Map<string, Array<Map<"destinationRangeStart" | "sourceRangeStart" | "rangeLength", number>>>());
+    }
 
-  console.log({ map });
+    const [destinationRangeStart, sourceRangeStart, rangeLength] = currentRow
+      .split(/\s/)
+      .map(Number);
 
-  return 0;
+    const currentMapKeyMaps = map.get(currentSeedMapType);
+
+    if (!currentMapKeyMaps) throw new Error("Unreachable");
+
+    currentMapKeyMaps.push(
+      new Map([
+        [SEED_MAP_KEYS.DESTINATION_RANGE_START, destinationRangeStart],
+        [SEED_MAP_KEYS.SOURCE_RANGE_START, sourceRangeStart],
+        [SEED_MAP_KEYS.RANGE_LENGTH, rangeLength],
+      ])
+    );
+
+    return map;
+  }, new Map<SeedMapType, Array<SeedMap>>());
+}
+
+export function getDestinationFromSource(
+  source: number,
+  sourceToDestinationMaps: Array<SeedMap>
+) {
+  return sourceToDestinationMaps.reduce(
+    (destination, sourceToDestinationMap) => {
+      const [destinationRangeStart, sourceRangeStart, rangeLength] =
+        sourceToDestinationMap.values();
+
+      if (isBetween(source, sourceRangeStart, sourceRangeStart + rangeLength)) {
+        return destination + (destinationRangeStart - sourceRangeStart);
+      }
+
+      return destination;
+    },
+    source
+  );
+}
+
+function isBetween(number: number, start: number, end: number) {
+  return start <= number && number < end;
 }
